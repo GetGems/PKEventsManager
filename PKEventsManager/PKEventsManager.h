02e4b8114cf0639e1b5d8246c66b2de2179a8e55
Eran Israel <eran@paykey.com>
2020-06-09 09:18:18 +0300

    Initial project setup

---
+++ PKEventsManager/PKEventsManager.h
@@ -0,0 +1,19 @@
+//
+//  PKEventsManager.h
+//  PKEventsManager
+//
+//  Created by Eran Israel on 09/06/2020.
+//  Copyright © 2020 Paykey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PKEventsManager.
+FOUNDATION_EXPORT double PKEventsManagerVersionNumber;
+
+//! Project version string for PKEventsManager.
+FOUNDATION_EXPORT const unsigned char PKEventsManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKEventsManager/PublicHeader.h>
+
+
---
+++ PKEventsManagerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PKEventsManagerExample
+//
+//  Created by Eran Israel on 09/06/2020.
+//  Copyright © 2020 Paykey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
