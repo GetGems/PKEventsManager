d3a1cdd66a95099f1a9809931203f66ae87f469d
Eran Israel <eran@paykey.com>
2020-06-09 14:51:16 +0300

    Add Proper Example project

--- PKEventsManagerExample/PKEventsManagerExample/ViewController.swift
+++ PKEventsManagerExample/PKEventsManagerExample/ViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import PKEventsManager
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        EventManager.shared.initialize(token: "")
     }
-
-
 }
 
